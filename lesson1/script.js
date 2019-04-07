25bcb2771ae611e5f02f50491d3ef5ecb0adcd01
mbr0969 <mbr0969@gmail.com>
2019-04-07 13:34:50 +0300

    Первое задание.

--- lesson1/script.js
+++ lesson1/script.js
@@ -13,11 +13,11 @@ time = prompt('Введите дату в формате YYYY-MM-DD',
         new Date().toISOString().slice(0, 10) );
 
 quest1 = prompt('Введите обязательную статью расходов в этом месяце','');
-appData.expenses[quest1] = prompt('Во сколько обойдется?', '');
+appData.expenses.quest1 = prompt('Во сколько обойдется?', '');
 
 quest2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
-appData.expenses[quest2] = prompt('Во сколько обойдется?', '');
-
+appData.expenses.quest2 = prompt('Во сколько обойдется?', '');
+alert(`Вы можете потратить в день: ${appData.budget / 30} `);
 alert(`Вам понадобится ${appData.expenses[quest1]/30} 
 рублей в день для статьи расходов ${quest1} и 
 ${appData.expenses[quest2]/30} рублей в день для ${quest2} статьи расходов` );
