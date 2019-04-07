22022c4ef694c80e9567cc30590ba0bba346a27e
mbr0969 <mbr0969@gmail.com>
2019-04-07 13:55:20 +0300

    Первое задание.

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,23 +1,29 @@
 'use strict';
 
-let money , time,quest1, quest2;
-let appData = {budget:money, 
-  timeData:time,
+let money, time, item1, item2;
+let appData = { 
+  budget: money, 
+  timeData: time,
    expenses: {}, 
    optionalExpenses:'', 
    income: [], 
-   savings: false};
+   savings: false
+};
 
 money = prompt('Ваш бюджет на месяц?', '');
 time = prompt('Введите дату в формате YYYY-MM-DD', 
         new Date().toISOString().slice(0, 10) );
+appData.budget = money;        
+//console.log(appData.budget);
 
-quest1 = prompt('Введите обязательную статью расходов в этом месяце','');
-appData.expenses.quest1 = prompt('Во сколько обойдется?', '');
+item1 = prompt('Введите обязательную статью расходов в этом месяце','');
+appData.expenses.budget1 = prompt('Во сколько обойдется?', '');
 
-quest2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
-appData.expenses.quest2 = prompt('Во сколько обойдется?', '');
-alert(`Вы можете потратить в день: ${appData.budget / 30} `);
-alert(`Вам понадобится ${appData.expenses[quest1]/30} 
-рублей в день для статьи расходов ${quest1} и 
-${appData.expenses[quest2]/30} рублей в день для ${quest2} статьи расходов` );
+item2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
+appData.expenses.budget2 = prompt('Во сколько обойдется?', '');
+
+alert(`Вы можете потратить в день: ${appData.budget/30} `);
+
+alert(`Вам понадобится ${appData.expenses.budget1/30} 
+рублей в день для статьи расходов ${item1} и 
+${appData.expenses.budget2/30} рублей в день для ${item2} статьи расходов` );
