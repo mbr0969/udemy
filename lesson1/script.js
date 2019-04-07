73f90c1b61faa5899720094ed909d11726d76028
mbr0969 <mbr0969@gmail.com>
2019-04-07 21:49:05 +0300

    Второе задание.

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money, time, item1, item2;
+let money, time;
 let appData = { 
   budget: money, 
   timeData: time,
@@ -10,20 +10,38 @@ let appData = {
    savings: false
 };
 
-money = prompt('Ваш бюджет на месяц?', '');
+money = +prompt('Ваш бюджет на месяц?', '');
 time = prompt('Введите дату в формате YYYY-MM-DD', 
         new Date().toISOString().slice(0, 10) );
+
 appData.budget = money;        
-//console.log(appData.budget);
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+        b = prompt('Во сколько обойдется?', '');
 
-item1 = prompt('Введите обязательную статью расходов в этом месяце','');
-appData.expenses.budget1 = prompt('Во сколько обойдется?', '');
+    if ((typeof(a)) === 'string' &&  (typeof(a)) != null && 
+        (typeof(b)) != null &&  a !='' && b!='' && a.length <50 ) {
+             console.log("done");
+        appData.expenses[a] = b;  
+    }
+//     else{
+//         i--;
+//         continue;
+//     }
+}
+appData.moneyPerDay = appData.budget/30;
+alert(appData.moneyPerDay);
 
-item2 = prompt('Введите вторую обязательную статью расходов в этом месяце','');
-appData.expenses.budget2 = prompt('Во сколько обойдется?', '');
+if (appData.moneyPerDay < 1000) {
+        console.log("Минимальный уровень достатка");
+}else if (appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000){
+        console.log("Высокий уровень достатка"); 
+}else{
+        console.log("Высокий уровень достатка");
+}
 
-alert(`Вы можете потратить в день: ${appData.budget/30} `);
-
-alert(`Вам понадобится ${appData.expenses.budget1/30} 
-рублей в день для статьи расходов ${item1} и 
-${appData.expenses.budget2/30} рублей в день для ${item2} статьи расходов` );
+// alert(`Вам понадобится ${appData.expenses.budget1/30} 
+// рублей в день для статьи расходов ${item1} и 
+// ${appData.expenses.budget2/30} рублей в день для ${item2} статьи расходов` );
---
+++ lesson_2/script.js
@@ -0,0 +1,23 @@
+// if(2*2 == 8){
+//  console.log('Верно!');
+// }else{
+//   console.log('Неверно!');
+//  }
+//switch(num){
+  //   case 60:
+  //     console.log('Верно!');
+  //     break;
+  // }
+  
+//   alert( "1"[0] );
+// let num = 60;
+// while (num < 65) {
+//   console.log(num);
+//   num++;    
+// }
+
+// for (let index = 1; index < 8; index++){
+//     console.log(index);
+    
+// }
+ 
